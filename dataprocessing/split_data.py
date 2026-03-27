4cbeda12810ea3cfa5bf89ff03e27446c784d7ba
KongeGeden <54670774+KongeGeden@users.noreply.github.com>
2026-03-27 12:33:16 +0100

    Change CSV file path to relative
    Updated the path for reading the CSV file to a relative path.

--- dataprocessing/split_data.py
+++ dataprocessing/split_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-data=pd.read_csv("E:\KU_MachineLearning_Study\Fake_news_project\Group-Fake-News-Project\99950stemmedOvr.csv")
+data=pd.read_csv("99950stemmedOvr.csv")
 data=data.sample(frac=1,random_state=0)
 trainset=data.iloc[:int(len(data)*0.80)]
 validation=data.iloc[int(len(data)*0.80):int(len(data)*0.90)]
